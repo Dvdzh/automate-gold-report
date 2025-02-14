b10cbdc28c2cf11f40e519f1ae2c0b7a6a705fd7
David Zhu <davidzhu@MacBook-Pro-de-David.local>
2025-02-14 10:29:17 -0500

    minor fix

--- test/test_generator.sh
+++ test/test_generator.sh
@@ -1,18 +1,29 @@
 if [ -d ".venv" ]; then
-    echo "Le dossier .venv existe."
+    echo "virtualenv does exist."
     source .venv/bin/activate
 else
-    echo "Le dossier .venv n'existe pas."
+    echo "virtualenv does not exist. Creating..."
     python3 -m venv .venv
     source .venv/bin/activate
 fi
 
+echo "Downloading requirements..."
 pip install -r requirements.txt
 
-for year in 2022 2023 2024
+# for year in 2022 2023 2024
+# do
+#     for week in {1..2}
+#     do
+#         echo "Generating week $week of year $year..."
+#         python3 src/generator.py $year $week
+#     done
+# done
+
+for year in 2025
 do
-    for week in {1..2}
+    for week in {1..6}
     do
+        echo "Generating week $week of year $year..."
         python3 src/generator.py $year $week
     done
 done
